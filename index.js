596fe5a4fe11d5e7bf46dbc7550505c32f6d7e9c
LucasOliveiraDantas <lucasdeoliveiradantas@protonmail.com>
2022-10-25 16:17:17 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function menuOn(){
+	const menuMobile = document.querySelector('.menu-nav')
+
+	menuMobile.classList.remove('off')
+	
+
+	
+}
+function menuOff(){
+	const menuMobile = document.querySelector('.menu-nav')
+
+	menuMobile.classList.add('off')
+}
